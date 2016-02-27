0f17fda90872a8b63eae71789227ade25a46b2b2
Brian Stovall <hoppenmaus@gmail.com>
2016-02-27 17:15:04 -0500

    Added 20 public domain books and a route for choosing from them.

--- src/compojure_test/handler.clj
+++ src/compojure_test/handler.clj
@@ -22,28 +22,88 @@
                [:pre [:p (files->story
                           500
                           "sample-text/grimm.txt"
-                          "sample-text/alice.txt"
-                          "sample-text/marx.txt")]]))
+                          "sample-text/alice.txt")]]))
 
     (GET "/mix-story" []
        (hic-p/html5
-        [:h3 "Choose stories from the library"]
+        [:head  (hic-p/include-css "/css/story.css")]
+        [:h3 "Choose stories from the library:"]
         (hic-f/form-to [:post "/my-story"]
                        (hic-f/label :alice "Alice in Wonderland")
                        (hic-f/check-box :alice false "sample-text/alice.txt")
+                       [:br]
+                       (hic-f/label :sherlock "The Adventures of Sherlock Holmes")
+                       (hic-f/check-box :sherlock false "sample-text/sherlock.txt")
+                       [:br]
+                       (hic-f/label :conneticut "A Conneticut Yankee in King Arthur's Court")
+                       (hic-f/check-box :conneticut false "sample-text/conneticut.txt")
+                       [:br]
+                       (hic-f/label :critique "A Critique of Pure Reason")
+                       (hic-f/check-box :critique false "sample-text/critique.txt")
+                       [:br]
+                       (hic-f/label :divine "The Divine Comedy")
+                       (hic-f/check-box :divine false "sample-text/divine.txt")
+                       [:br]
+                       (hic-f/label :dunwich "The Dunwich Horror")
+                       (hic-f/check-box :dunwich false "sample-text/dunwich.txt")
+                       [:br]
+                       (hic-f/label :ethics "Ethics")
+                       (hic-f/check-box :ethics false "sample-text/ethics.txt")
+                       [:br]
                        (hic-f/label :grimm "Grimm Fairytales")
                        (hic-f/check-box :grimm false "sample-text/grimm.txt")
-                       (hic-f/label :marx "Karl Marx")
-                       (hic-f/check-box :marx false "sample-text/marx.txt")
+                       [:br]
+                       (hic-f/label :macbeth "Macbeth")
+                       (hic-f/check-box :macbeth false "sample-text/macbeth.txt")
+                       [:br]
+                       (hic-f/label :man "The Man With Two Left Feet")
+                       (hic-f/check-box :man false "sample-text/man.txt")
+                       [:br]
+                       (hic-f/label :munch "Mr. Munchausen")
+                       (hic-f/check-box :munch false "sample-text/munchausen.txt")
+                       [:br]
+                       (hic-f/label :mysticism "Mysticism and Logic")
+                       (hic-f/check-box :mysticism false "sample-text/mysticism.txt")
+                       [:br]
+                       (hic-f/label :nonsense "Nonsense Books")
+                       (hic-f/check-box :nonsense false "sample-text/nonsense.txt")
+                       [:br]
+                       (hic-f/label :paradise "Paradise Lost")
+                       (hic-f/check-box :paradise false "sample-text/paradise.txt")
+                       [:br]
+                       (hic-f/label :poe "The Works of Edgar Allen Poe: Vol. One")
+                       (hic-f/check-box :poe false "sample-text/poe.txt")
+                       [:br]
+                       (hic-f/label :pride "Pride and Prejudice")
+                       (hic-f/check-box :pride false "sample-text/pride.txt")
+                       [:br]
+                       (hic-f/label :qix "Don Quixote")
+                       (hic-f/check-box :qix false "sample-text/qix.txt")
+                       [:br]
+                       (hic-f/label :tale "A Tale of Two Cities")
+                       (hic-f/check-box :tale false "sample-text/tale.txt")
+                       [:br]
+                       (hic-f/label :trial "The Trial")
+                       (hic-f/check-box :trial false "sample-text/trial.txt")
+                       [:br]
+                       (hic-f/label :white "White Nights")
+                       (hic-f/check-box :white false "sample-text/white.txt")
+                       [:br]
                        (r-a-f/anti-forgery-field)
                        (hic-f/submit-button "Write it!"))))
 
     (POST "/my-story"
           req
           (hic-p/html5
+           [:head  (hic-p/include-css "/css/story.css")]
            [:pre [:p (apply files->story
                       500
-                      (vals (select-keys (:params req) [:alice :marx :grimm])))]]))
+                      (vals (select-keys (:params req)
+                                           [:alice :sherlock :conneticut :critique
+                                            :divine :qix :dunwich :ethics :grimm
+                                            :macbeth :man :munch :mysticism :nonsense
+                                            :paradise :poe :pride :qix :tale
+                                            :trial :white])))]]))
   
   (route/not-found "Not Found"))
 
