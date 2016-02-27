803b9c9101e370a2fbdb706f1503e7e47c602a8f
Brian Stovall <hoppenmaus@gmail.com>
2016-02-26 20:12:04 -0500

    built stub front-page, demo-page and began upload-page

--- src/compojure_test/handler.clj
+++ src/compojure_test/handler.clj
@@ -2,18 +2,38 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as hic-p]))
+            [ring.util.anti-forgery :as r-a-f]
+            [hiccup.page :as hic-p]
+            [hiccup.element :as hic-e]
+            [hiccup.form :as hic-f]))
 
 (load "generator")
 (use 'markov-twister.generator)
  
 (defroutes app-routes
   (GET "/" [] (hic-p/html5
+               [:h2 "Places you can go:"]
+               (hic-e/link-to "/demo" [:p "Demo story from Grimm, Alice, and Marx"])
+               (hic-e/link-to "/upload-files" [:p "Upload your own files to make a story."])))
+
+  (GET "/upload-files" [] (hic-p/html5
+                           (hic-f/form-to [:post "/make-story"]
+                                          (hic-f/label :file "Choose a file:")
+                                          (hic-f/file-upload :file)
+                                          (r-a-f/anti-forgery-field)
+                                          (hic-f/submit-button "Write it!"))))
+
+  (POST "/make-story" []
+        (hic-p/html5
+         [:h2 "Success"]))
+  
+  (GET "/demo" [] (hic-p/html5
                [:head  (hic-p/include-css "/css/story.css")]
-               [:h3 "Welcome!"]
-               [:pre [:p (files->story 500
-                                       "sample-text/grimm.txt"
-                                       "sample-text/alice.txt")]]))
+               [:pre [:p (files->story
+                          500
+                          "sample-text/grimm.txt"
+                          "sample-text/alice.txt"
+                          "sample-text/marx.txt")]]))
   
   (route/not-found "Not Found"))
 
