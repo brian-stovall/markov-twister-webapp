184b9ff662947bc98864867c9f706b8e1b18cc52
Brian Stovall <hoppenmaus@gmail.com>
2016-02-27 08:48:30 -0500

    wrote route that allows user to select texts from a library to mix

--- src/compojure_test/handler.clj
+++ src/compojure_test/handler.clj
@@ -15,21 +15,8 @@
   (GET "/" [] (hic-p/html5
                [:h2 "Places you can go:"]
                (hic-e/link-to "/demo" [:p "Demo story from Grimm, Alice, and Marx"])
-               (hic-e/link-to "/upload-files" [:p "Upload your own files to make a story."])))
+               (hic-e/link-to "/mix-story" [:p "Choose files from our library to make a story."])))
 
-  (GET "/upload-files" [] (hic-p/html5
-                           (hic-f/form-to [:post "/make-story"]
-                                          (hic-f/label :file "Choose a file:")
-                                          (hic-f/file-upload :file)
-                                          (r-a-f/anti-forgery-field)
-                                          (hic-f/submit-button "Write it!"))))
-
-  (POST "/make-story"
-        [req]
-        ;(io/copy (:body req) (io/output-stream (io/file "body.txt")))
-        (hic-p/html5
-         [:p (str (:body req))]))
-  
   (GET "/demo" [] (hic-p/html5
                [:head  (hic-p/include-css "/css/story.css")]
                [:pre [:p (files->story
@@ -37,6 +24,26 @@
                           "sample-text/grimm.txt"
                           "sample-text/alice.txt"
                           "sample-text/marx.txt")]]))
+
+    (GET "/mix-story" []
+       (hic-p/html5
+        [:h3 "Choose stories from the library"]
+        (hic-f/form-to [:post "/my-story"]
+                       (hic-f/label :alice "Alice in Wonderland")
+                       (hic-f/check-box :alice false "sample-text/alice.txt")
+                       (hic-f/label :grimm "Grimm Fairytales")
+                       (hic-f/check-box :grimm false "sample-text/grimm.txt")
+                       (hic-f/label :marx "Karl Marx")
+                       (hic-f/check-box :marx false "sample-text/marx.txt")
+                       (r-a-f/anti-forgery-field)
+                       (hic-f/submit-button "Write it!"))))
+
+    (POST "/my-story"
+          req
+          (hic-p/html5
+           [:pre [:p (apply files->story
+                      500
+                      (vals (select-keys (:params req) [:alice :marx :grimm])))]]))
   
   (route/not-found "Not Found"))
 
